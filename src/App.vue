736fe2526dcefefc6ba1a8bdf19f71c880e07250
njgheorghita <nickgheorghita@gmail.com>
2024-12-04 15:26:40 -0800

    pr cleanup

--- src/App.vue
+++ src/App.vue
@@ -18,31 +18,29 @@ onMounted(async () => {
   if (update) {
     toast({
   	  title: 'Update available',
-  	  description: `An update to version ${update.version} is available. Click here to install it.`,
+  	  description: `An update to version ${update.version} is available.`,
 	  variant: 'info',
-	  //onClick: () => installUpdate(update)
     });
-//  let downloaded = 0;
- // let contentLength = 0;
-  // alternatively we could also call update.download() and update.install() separately
-  //await update.downloadAndInstall((event) => {
-   // switch (event.event) {
-    //  case 'Started':
-     //   contentLength = event.data.contentLength;
-      //  console.log(`started downloading ${event.data.contentLength} bytes`);
-       // break;
-      //case 'Progress':
-       // downloaded += event.data.chunkLength;
-        //console.log(`downloaded ${downloaded} from ${contentLength}`);
-        //break;
-      //case 'Finished':
-       // console.log('download finished');
-        //break;
-   // }
- // });
+  let downloaded = 0;
+  let contentLength = 0;
+  await update.downloadAndInstall((event) => {
+    switch (event.event) {
+      case 'Started':
+        contentLength = event.data.contentLength;
+        console.log(`started downloading ${event.data.contentLength} bytes`);
+        break;
+      case 'Progress':
+        downloaded += event.data.chunkLength;
+        console.log(`downloaded ${downloaded} from ${contentLength}`);
+        break;
+      case 'Finished':
+        console.log('download finished');
+        break;
+    }
+  });
 
-  //console.log('update installed');
-  //await relaunch();
+  console.log('update installed');
+  await relaunch();
   }
 
 
