b76997b96a907dd9b97eb223c2f3f011b166e58f
Michael Green <michaelgreen06@gmail.com>
2024-12-16 15:09:00 +0700

    added account tab

---
+++ src/pages/Account.vue
@@ -0,0 +1,17 @@
+<template>
+    <PageLayout>
+      <Card>
+        <CardHeader>
+          <CardTitle>Account Information</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <!-- Account lookup form will go here -->
+        </CardContent>
+      </Card>
+    </PageLayout>
+  </template>
+  
+  <script setup>
+  import PageLayout from '@/components/layouts/PageLayout.vue'
+  import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+  </script>
